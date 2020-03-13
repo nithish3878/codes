0a04d81ebd082aee749b290af6467e68698fe281
nithish3878 <61942322+nithish3878@users.noreply.github.com>
2020-03-13 19:44:21 +0530

    Update week1prob6.py

--- week1prob6.py
+++ week1prob6.py
@@ -1,6 +1,11 @@
-from collections import Counter
+#Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.from collections import Counter
+
 nums=list(map(int,input().split()))
 dict=Counter(nums)
 for i in nums:
     if(dict[i]==1):
         print(i)
+   
+#Input: [2,2,3,2]
+#Output: 3
+
